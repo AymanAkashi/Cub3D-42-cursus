0005b8abd7f6b6e05213c30fe7c314b38bf8c3b8
AymanAkashi <aaggoujj@student.1337.ma>
2022-11-08 13:43:02 +0100

    feat : Add landing page

--- src/begin_cub.c
+++ src/begin_cub.c
@@ -6,7 +6,7 @@
 /*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 09:56:14 by aaggoujj          #+#    #+#             */
-/*   Updated: 2022/11/07 16:41:12 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2022/11/08 13:32:51 by aaggoujj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,97 @@ int	create_trgb(int t, t_color color)
 	return (t << 24 | color._r << 16 | color._g << 8 | color._b);
 }
 
+int mouse_click(int button, int x, int y, t_cub *cub)
+{
+	(void)x;
+	(void)y;
+	if (button == 1 && !cub->mouse_hide)
+	{
+		mlx_mouse_hide();
+		cub->mouse_hide = _true;
+	}
+	return (0);
+}
+
+void	hook_start(t_cub *cub)
+{
+	cub->img = mlx_new_image(cub->mlx, WIN_WIDTH, WIN_HEIGHT);
+	cub->addr = (unsigned int *) mlx_get_data_addr(cub->img, &cub->bpp,
+			&cub->size_line, &cub->endian);
+	set_hide_mouse(KEY_Q,cub);
+	mlx_hook(cub->win, 17, 0, end_game, cub);
+	mlx_hook(cub->win, 02, 1L << 0, input, cub);
+	mlx_hook(cub->win, 03, 1L << 1, input_release, cub);
+	mlx_hook(cub->win, 06, 1L << 6, mouse_move, cub);
+	mlx_mouse_hook(cub->win, mouse_click, cub);
+	mlx_loop_hook(cub->mlx, loop, cub);
+}
+
+int mouse_start(int button, int x, int y, t_cub *cub)
+{
+	int width;
+	int height;
+
+	width = WIN_WIDTH / 2;
+	height = WIN_HEIGHT / 2;
+	if (button == 1 && (x >= width - 200 && x <= width + 200) && (y >= height - 100 && y <= height))
+	{
+		hook_start(cub);
+	}
+	if (button == 1 && (x >= width - 110 && x <= width + 110) && (y >= height + 40  && y <= height + 80))
+	{
+		end_game(cub);
+	}
+	return (0);
+}
+
+int mouse_init(t_cub *cub)
+{
+	if (mlx_mouse_hook(cub->win, mouse_start, cub) == 0)
+		return (0);
+	return (1);
+}
+
+void	open_image(t_cub *cub, t_compass *tmp)
+{
+	tmp->img = mlx_xpm_file_to_image(cub->mlx, tmp->path, &cub->compass->width, &cub->compass->height);
+	if (!tmp->img)
+	{
+		red();printf("Cube3D: Error file doesn't open\n");reset();
+		exit(1);
+	}
+}
+
+void	set_image(t_compass *list, t_cub *cub)
+{
+	t_compass	*tmp;
+
+	tmp = list;
+	while(tmp)
+	{
+		open_image(cub, tmp);
+		tmp = tmp->next;
+	}
+}
+
+int	init_game(t_cub *cub)
+{
+	void	*img;
+	void	*image;
+	int     width;
+	int     height;
+
+	height = 0;
+	width = 0;
+	img	= mlx_new_image(cub->mlx, WIN_WIDTH, WIN_HEIGHT);
+	image = mlx_xpm_file_to_image(cub->mlx, "textures/play.xpm", &width, &height);
+	mlx_put_image_to_window(cub->mlx, cub->win, image, 0, 0);
+	mlx_hook(cub->win, 02, 0, input, cub);
+	if (mlx_mouse_hook(cub->win, mouse_start, cub) == 0)
+		return (1);
+	return (0);
+}
+
 void	begin_cub(t_cub *cub)
 {
 	get_win_size(cub);
@@ -102,15 +193,7 @@ void	begin_cub(t_cub *cub)
 	cub->player->pos.y = cub->player->pos.y * BLOCK_SIZE + BLOCK_SIZE / 2;
 	cub->mlx = mlx_init();
 	cub->win = mlx_new_window(cub->mlx, WIN_WIDTH, WIN_HEIGHT, "cub3D");
-	cub->img = mlx_new_image(cub->mlx, WIN_WIDTH, WIN_HEIGHT);
-	cub->addr = (unsigned int *) mlx_get_data_addr(cub->img, &cub->bpp,
-			&cub->size_line, &cub->endian);
-	set_hide_mouse(KEY_Q,cub);
-	mlx_hook(cub->win, 17, 0, end_game, cub);
-	mlx_hook(cub->win, 02, 1L << 0, input, cub);
-	mlx_hook(cub->win, 03, 1L << 1, input_release, cub);
-	mlx_hook(cub->win, 06, 1L << 6, mouse_hook, cub);
-	mlx_hook(cub->win, 5, 0, mouse_release, cub);
-	mlx_loop_hook(cub->mlx, loop, cub);
+	set_image(cub->compass, cub);
+	init_game(cub);
 	mlx_loop(cub->mlx);
 }
